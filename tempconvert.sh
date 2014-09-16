5e5a5512d0cf0b610e1b410135e01954da3fb700
avannuland <amvan21@ksu.edu>
2014-09-15 21:19:26 -0500

    Add R and K as output

--- tempconvert.sh
+++ tempconvert.sh
@@ -9,11 +9,15 @@ else
   if [ $2 == "F" ];
     then
 	((temperature = ($1-32)*5/9))
+	((temperatureK = temperature+273))
     echo "Temperature = $temperature C"
+    echo "Temperature = $temperatureK K"
   elif [ $2 == "C" ];
     then
 	((temperature = ($1*9/5)+32))
+	((temperatureR = temperature+460))
     echo "Temperature = $temperature F"
+    echo "Temperature = $temperatureR R"
   else
     echo "Invalid units - please enter F or C"
   fi
